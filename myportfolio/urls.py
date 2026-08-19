27751ae523e2ea823ed1698617a23a98f990960b
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-08-19 09:34:44 +0530

    final fixes

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -19,10 +19,7 @@ from django.contrib.admin import views
 from django.urls import include, path
 
 
-from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('baseapp.urls')),  # Connects baseapp URLs to the root domain
+    path('', include('baseapp.urls')),
 ]
